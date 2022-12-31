ea3d338f6bdfe684589db785931e0b26974cea87
Mostafa Elshazly <mostafa.elshazly@anoda.mobi>
2022-12-31 09:55:48 +0200

    Add Firebase framework

---
+++ eMasaryTask/Services/Firebase/FirebaseManager.swift
@@ -0,0 +1,16 @@
+//
+//  FirebaseManager.swift
+//  eMasaryTask
+//
+//  Created by Mostafa Elshazly on 31/12/2022.
+//
+
+import Foundation
+import FirebaseCore
+
+class FirebaseManager {
+
+    static func configureFirebase() {
+        FirebaseApp.configure()
+    }
+}
